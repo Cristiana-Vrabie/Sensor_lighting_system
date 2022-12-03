48529667d8d26172a79818e25075d304d17150ee
Cristiana-Vrabie <56795332+Cristiana-Vrabie@users.noreply.github.com>
2022-12-03 17:09:02 +0200

    Added OS, v1

--- sketch_oct09b.ino
+++ sketch_oct09b.ino
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                 Includes                                   */
 /*----------------------------------------------------------------------------*/
-//#include <Arduino_FreeRTOS.h>
+#include <Arduino_FreeRTOS.h>
 
 /*----------------------------------------------------------------------------*/
 /*                               Local defines                                */
@@ -25,7 +25,10 @@
 /*                             Global data at RAM                             */
 /*----------------------------------------------------------------------------*/
 long distance_cm;
-long light_intensity;
+long light_intensity_percentage;
+bool system_controlled_led_status = false;
+bool cloud_controlled_les_status = false;
+
 /*----------------------------------------------------------------------------*/
 /*                       Declaration of local functions                       */
 /*----------------------------------------------------------------------------*/
@@ -35,20 +38,23 @@ long light_intensity;
  * \param     echo_Pin: represents the HC-SR04 sensor echo pin
  * \return    The measured distance in cm 
  */
- int SDTR_MeasureDistance(int trigger_Pin, int echo_Pin);
+void SDTR_MeasureDistance(void *pvParameters);
   /**
  * \brief     This function is used for reading an analog value;
  * \param     input_Pin: represents the pin used for reading the ADC value
  * \return    Light intensity
  */
- long SDTR_FindLightIntensity(int input_Signal_Pin);
+void SDTR_FindLightIntensity(void *pvParameters);
   /**
  * \brief     This function is used for turning on/off a led;
  * \param     pin_Led: represents the IO microcontroller pin
  * \param     target_distance: threshold
  * \return    void 
  */
- void SDTR_BlinkLed(int red_pin, int green_pin, int blue_pin, int threshold_distance, int threshold_light_intensity);
+ void SDTR_BlinkLed(void *pvParameters);
+ void SDTR_GetDistance();
+ void SDTR_GetIntenisty();
+void SDTR_Blink();
 
 /*----------------------------------------------------------------------------*/
 /*                     Implementation of global functions                     */
@@ -59,82 +65,153 @@ long light_intensity;
  * \return    None 
  */
 void setup() {
+
+  // Initialize serial and wait for port to open:
+  Serial.begin(9600);
+
+  //Pin settings 
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT); 
   pinMode(TEMT_PIN, INPUT);
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);    
-  Serial.begin(9600);
+
+  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+  vTaskDelay(1500); 
+
+  //Task creation
+ /* xTaskCreate(
+    SDTR_UpdateCloud,            // Task function
+    "SDTR_UpdateCloud",          // Name 
+    10000,                       // Stack size of the task
+    NULL,                        // Parameters
+    1,                           // Priority
+    NULL);                       // Task handle
+  */
+  xTaskCreate(
+    SDTR_MeasureDistance,        // Task function
+    "SDTR_MeasureDistance",      // Name 
+    10000,                       // Stack size of the task
+    NULL,                        // Parameters
+    1,                           // Priority
+    NULL);                       // Task handle
+    
+  xTaskCreate(
+    SDTR_FindLightIntensity,        // Task function
+    "SDTR_FindLightIntensity",      // Name 
+    10000,                       // Stack size of the task
+    NULL,                        // Parameters
+    1,                           // Priority
+    NULL);                       // Task handle
+
+  xTaskCreate(
+    SDTR_BlinkLed,        // Task function
+    "SDTR_BlinkLed",      // Name 
+    10000,                       // Stack size of the task
+    NULL,                        // Parameters
+    1,                           // Priority
+    NULL);                       // Task handle
 }
 
 void loop() {
-  distance_cm = SDTR_MeasureDistance(TRIGGER_PIN, ECHO_PIN);
-  Serial.print("Distance is: ");
-  Serial.println(distance_cm);  
-  Serial.println();  
-  light_intensity = SDTR_FindLightIntensity(TEMT_PIN);
-  Serial.print("Light intensity is: ");
-  Serial.println(light_intensity);  
-  Serial.println();
-  SDTR_BlinkLed(RED_PIN, GREEN_PIN, BLUE_PIN, MAX_DISTANCE, MIN_LIGHT_INTENSITY);
-  delay(100);
+  //empty
 }
 
 /*----------------------------------------------------------------------------*/
 /*                     Implementation of local functions                      */
 /*----------------------------------------------------------------------------*/
+
 /*----------------------------------------------------------------------------*/
 /* SDTR_MeasureDistance                                                      */
 /*----------------------------------------------------------------------------*/
-int SDTR_MeasureDistance(int trigger_Pin, int echo_Pin) 
-{
-  long pulseWidthInMicro = 0;
-  long distance = 0;
-  
-  digitalWrite(trigger_Pin, HIGH);
-  delayMicroseconds(10);  
-  digitalWrite(trigger_Pin, LOW);
-  pulseWidthInMicro = pulseIn(echo_Pin, HIGH);
-  distance = (pulseWidthInMicro*340/10000)/2; //cm
-  //distanta = (durata impuls*0,034secunde)/2
-  return distance;
+
+void SDTR_MeasureDistance(void *pvParameters) {
+  (void) pvParameters;
+
+  // A task shall never return or exit
+  for(;;)
+
+  Serial.println("READING DISTANCE SENSOR VALUE");
+  SDTR_GetDistance();
 }
+
 /*----------------------------------------------------------------------------*/
-/* SDTR_MeasureDistance                                                      */
+/* SDTR_GetDistance                                                     */
 /*----------------------------------------------------------------------------*/
-long SDTR_FindLightIntensity(int input_Signal_Pin)
-{
+void SDTR_GetDistance() {
+  long pulseWidthInMicro = 0;
+  
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);  
+  digitalWrite(TRIGGER_PIN, LOW);
+  pulseWidthInMicro = pulseIn(ECHO_PIN, HIGH);
+  distance_cm = (pulseWidthInMicro*340/10000)/2; //cm
+  //distanta = (durata impuls*0,034secunde)/2
+  Serial.print("Distance: ");
+  Serial.print(distance_cm);
+  Serial.print(" cm");
+}
+
+/*----------------------------------------------------------------------------*/
+/* SDTR_FindLightIntensity                                                     */
+/*----------------------------------------------------------------------------*/
+void SDTR_FindLightIntensity(void *pvParameters) {
+  (void) pvParameters;
+
+    // A task shall never return or exit
+  for(;;)
+
+  Serial.println("READING LIGHT INTENSITY SENSOR VALUE");
+  SDTR_GetIntensity();
+}
+
+/*----------------------------------------------------------------------------*/
+/* SDTR_GetIntensity                                                     */
+/*----------------------------------------------------------------------------*/
+void SDTR_GetIntensity() {
   long light_intensity_pin = 0; //pin light intensity
-  int light_intensity_percentage = 0;
   long sum = 0;
   int adc_sample = 0;
 
   for(adc_sample = 0; adc_sample < NUMBER_OF_SAMPLES_AVERAGE; adc_sample++)
   {
-    sum = sum + analogRead(input_Signal_Pin);
+    sum = sum + analogRead(TEMT_PIN);
   }
 
   light_intensity_pin = sum / NUMBER_OF_SAMPLES_AVERAGE; 
   light_intensity_percentage = light_intensity_pin * 0.0977;
-
-  return light_intensity_percentage;
 }
+
 /*----------------------------------------------------------------------------*/
-/* SDTR_BlinkLed                                                             */
+/* SDTR_BlinkLed                                                    */
 /*----------------------------------------------------------------------------*/
- void SDTR_BlinkLed(int red_pin, int green_pin, int blue_pin, int threshold_distance, int threshold_light_intensity)
- {
-  if((distance_cm > MAX_DISTANCE) && (light_intensity < MIN_LIGHT_INTENSITY)) 
+void SDTR_BlinkLed(void *pvParameters) {
+  (void) pvParameters;
+
+  // A task shall never return or exit
+  for(;;)
+
+  SDTR_Blink();
+}
+
+/*----------------------------------------------------------------------------*/
+/* SDTR_Blink                                                 */
+/*----------------------------------------------------------------------------*/
+void SDTR_Blink() {
+  if((distance_cm > MAX_DISTANCE) && (light_intensity_percentage < MIN_LIGHT_INTENSITY)) 
   {
+    system_controlled_led_status = true;
     digitalWrite(RED_PIN, LOW);
     digitalWrite(BLUE_PIN, HIGH);
     digitalWrite(GREEN_PIN, HIGH);
   }
   else 
   {
+    system_controlled_led_status = false;
     digitalWrite(RED_PIN, LOW);
     digitalWrite(BLUE_PIN, LOW);
     digitalWrite(GREEN_PIN, LOW);    
   }
- }
+  vTaskDelay(20);
+}
